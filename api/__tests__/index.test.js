d71e2a17d6807a72f769f952f8b63df1c549d86d
Jochem Crab <jochemcrab14@gmail.com>
2021-11-13 14:07:01 +0100

    feature (test): setup for unittests

--- api/__tests__/index.test.js
+++ api/__tests__/index.test.js
@@ -1,1 +1,16 @@
-const { initTables } = require('../index')
+const { initTables, delCharacter, addCharacter, getAllCharacters, getCharacter } = require('../index')
+
+test("addCharacter -> Trying to add a character", () => {
+    const Character = {
+        ownerID: 24,
+        firstName: 'Cirilla',
+        lastName: 'Dash',
+        description: `Cirilla Dash, once beloved in the kingdom
+        of her parents, now left alone. She became a mercary for
+        hire at the age of 17 because of her vast magical powers.`,
+        characterRace: 'Eladrin',
+        characterClass: 'Sorcerer'
+    }
+    addCharacter(Character)
+
+});
